1f60980bb21df824a123816006785415cd88428d
Adam Bertram <adbertram@gmail.com>
2017-05-23 18:29:58 -0500

    renames to iperfautomate

--- buildscripts/build.ps1
+++ buildscripts/build.ps1
@@ -2,66 +2,12 @@ $ErrorActionPreference = 'Stop'
 
 try {
 
-	$manifestFilePath = "$env:APPVEYOR_BUILD_FOLDER\PSSoftware.psd1"
+	$manifestFilePath = "$env:APPVEYOR_BUILD_FOLDER\iPerfAutomate.psd1"
 	$manifestContent = Get-Content -Path $manifestFilePath -Raw
 
 	$functionsToExport = @(
-		'Compare-FilePath',
-		'Compare-FolderPath',
-		'Convert-CompressedGuidToGuid',
-		'Convert-GuidToCompressedGuid',
-		'Convert-ToUncPath',
-		'Copy-FileWithHashCheck',
-		'Find-InTextFile',
-		'Get-32BitProgramFilesPath',
-		'Get-32BitRegistrySoftwarePath',
-		'Get-AllUsersDesktopFolderPath',
-		'Get-AllUsersProfileFolderPath',
-		'Get-AllUsersRegistryValue',
-		'Get-AllUsersRegistryKey',
-		'Get-AllUsersStartMenuFolderPath',
-		'Get-Architecture',
-		'Get-ChildProcess',
-		'Get-DriveFreeSpace',
-		'Get-FileVersion',
-		'Get-InstalledSoftware',
-		'Get-InstallerType',
-		'Get-InstallshieldInstallString',
-		'Get-LoggedOnUserSID',
-		'Get-MyFileHash',
-		'Get-OperatingSystem',
-		'Get-RegistryValue',
-		'Get-RootUserProfileFolderPath',
-		'Get-Shortcut',
-		'Get-SystemTempFolderPath',
-		'Get-UserProfile',
-		'Get-UserProfilePath',
-		'Import-Certificate',
-		'Import-RegistryFile',
-		'Install-Software',
-		'Get-MsiexecInstallString',
-		'New-Shortcut',
-		'Register-File',
-		'Remove-MyService',
-		'Remove-ProfileItem',
-		'Remove-RegistryKey',
-		'Remove-Software',
-		'Set-AllUserStartupAction',
-		'Set-MyFileSystemAcl',
-		'Set-AllUsersRegistryValue',
-		'Start-Log',
-		'Stop-MyProcess',
-		'Stop-SoftwareProcess',
-		'Test-Process',
-		'Test-InstalledSoftware',
-		'Uninstall-InstallShieldPackage',
-		'Uninstall-ViaMsizap',
-		'Uninstall-WindowsInstallerPackage',
-		'Uninstall-WindowsInstallerPackageWithMsiexec',
-		'Uninstall-WindowsInstallerPackageWithMsiModule',
-		'Wait-MyProcess',
-		'Wait-WindowsInstaller',
-		'Write-Log'
+		'New-IperfSchedule',
+		'Start-IPerfMonitorTest'
 	)
 
 	## Update the module version based on the build version and limit exported functions
@@ -83,5 +29,6 @@ try {
 	Write-Host '=============================================='
 
 } catch {
+	Write-Error -Message $_.Exception.Message
 	$host.SetShouldExit($LastExitCode)
 }
--- buildscripts/publish.ps1
+++ buildscripts/publish.ps1
@@ -2,15 +2,15 @@ $ErrorActionPreference = 'Stop'
 
 try {
 	## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
-	$tempmoduleFolderPath = "$env:Temp\PSSoftware"
+	$tempmoduleFolderPath = "$env:Temp\iPerfAutomate"
 	$null = mkdir $tempmoduleFolderPath
 
 	## Move all of the files/folders to exclude out of the main folder
 	$excludeFromPublish = @(
-		'PSSoftware\\buildscripts'
-		'PSSoftware\\appveyor\.yml'
-		'PSSoftware\\\.git'
-		'PSSoftware\\README\.md'
+		'iPerfAutomate\\buildscripts'
+		'iPerfAutomate\\appveyor\.yml'
+		'iPerfAutomate\\\.git'
+		'iPerfAutomate\\README\.md'
 	)
 	$exclude = $excludeFromPublish -join '|'
 	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | where { $_.FullName -match $exclude } | Move-Item -Destination $env:temp
